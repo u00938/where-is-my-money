5ff95d773144f9ee65a4ba59b14ac4de17b0baa9
u00938 <yo01030184262@gmail.com>
2023-11-16 00:27:59 +0900

    fix: add stt per category caching

--- src/cache/cache.interface.ts
+++ src/cache/cache.interface.ts
@@ -2,3 +2,14 @@ export interface budgetStt {
   per: Array<string>,
   date: string
 }
+
+export interface category {
+  id: string,
+  name: string
+}
+
+export interface expandStt {
+  totalPer: string,
+  categoryPer: object,
+  date: string
+}
